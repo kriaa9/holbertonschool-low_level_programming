51f3f9b4b45afdb2acebd73813c6a14e1fd45fb8
kriaa9 <11596@holbertonstudents.com>
2025-10-14 17:09:22 +0100

    Fix task 4: Convert 4-space indentation to tabs

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 	if (letter != 'e' && letter != 'q')
-	    putchar(letter);
-    }
-    putchar('\n');
+		putchar(letter);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
