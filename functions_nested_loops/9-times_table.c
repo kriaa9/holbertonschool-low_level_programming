bbbe5246e3bc567d22be0a86c1c8fabab762331f
kriaa9 <11596@holbertonstudents.com>
2025-10-14 19:05:28 +0100

    Fix task 9: Use unexpand to ensure proper tabs

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,33 +7,35 @@
  */
 void times_table(void)
 {
-	int row, col, product;
+    int row, col, product;
 
-	for (row = 0; row <= 9; row++)
+    for (row = 0; row <= 9; row++)
+    {
+	for (col = 0; col <= 9; col++)
 	{
-		for (col = 0; col <= 9; col++)
+	    product = row * col;
+
+	    if (col == 0)
+	    {
+		_putchar('0');
+	    }
+	    else
+	    {
+		_putchar(',');
+		_putchar(' ');
+
+		if (product < 10)
 		{
-			product = row * col;
-			if (col == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (product < 10)
-				{
-				    _putchar(' ');
-				    _putchar(product + '0');
-				}
-				else
-				{
-				    _putchar((product / 10) + '0');
-				    _putchar((product % 10) + '0');
-				}
-			}
+		    _putchar(' ');
+		    _putchar(product + '0');
 		}
-		_putchar('\n');
+		else
+		{
+		    _putchar((product / 10) + '0');
+		    _putchar((product % 10) + '0');
+		}
+	    }
 	}
+	_putchar('\n');
+    }
 }
