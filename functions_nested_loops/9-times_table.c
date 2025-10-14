071a06467650556d962332458b224ad5113ef1f2
kriaa9 <11596@holbertonstudents.com>
2025-10-14 19:24:28 +0100

    Fix task 9: Convert spaces to tabs using unexpand

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,33 +7,33 @@
  */
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
 
-            if (col == 0)
-            {
-                _putchar(product + '0');
-            }
-            else if (product < 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
