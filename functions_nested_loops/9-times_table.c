f09b285c8a1cc8f02b6092bc87abc70705333fa8
kriaa9 <11596@holbertonstudents.com>
2025-10-14 19:18:15 +0100

    Fix task 9: Manual tab insertion for Betty

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table
+ * times_table - Prints the 9 times table, starting with 0
  *
  * Return: void
  */
@@ -11,31 +11,29 @@ void times_table(void)
 
     for (row = 0; row <= 9; row++)
     {
-	for (col = 0; col <= 9; col++)
-	{
-	    product = row * col;
+        for (col = 0; col <= 9; col++)
+        {
+            product = row * col;
 
-	    if (col == 0)
-	    {
-		_putchar('0');
-	    }
-	    else
-	    {
-		_putchar(',');
-		_putchar(' ');
-
-		if (product < 10)
-		{
-		    _putchar(' ');
-		    _putchar(product + '0');
-		}
-		else
-		{
-		    _putchar((product / 10) + '0');
-		    _putchar((product % 10) + '0');
-		}
-	    }
-	}
-	_putchar('\n');
+            if (col == 0)
+            {
+                _putchar(product + '0');
+            }
+            else if (product < 10)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(product + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar((product / 10) + '0');
+                _putchar((product % 10) + '0');
+            }
+        }
+        _putchar('\n');
     }
 }
