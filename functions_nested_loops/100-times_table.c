c3f95e24d97059e5ace1948f5d73ce2b722019e1
kriaa9 <11596@holbertonstudents.com>
2025-10-14 19:48:01 +0100

    Fix task 12: Use vim retab for guaranteed tabs

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,46 +8,46 @@
  */
 void print_times_table(int n)
 {
-    int row, col, product;
+	int row, col, product;
 
-    if (n < 0 || n > 15)
-	return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (row = 0; row <= n; row++)
-    {
-	for (col = 0; col <= n; col++)
+	for (row = 0; row <= n; row++)
 	{
-	    product = row * col;
+		for (col = 0; col <= n; col++)
+		{
+			product = row * col;
 
-	    if (col == 0)
-	    {
-		_putchar(product + '0');
-	    }
-	    else if (product < 10)
-	    {
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(product + '0');
-	    }
-	    else if (product < 100)
-	    {
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar((product / 10) + '0');
-		_putchar((product % 10) + '0');
-	    }
-	    else
-	    {
-		_putchar(',');
-		_putchar(' ');
-		_putchar((product / 100) + '0');
-		_putchar(((product / 10) % 10) + '0');
-		_putchar((product % 10) + '0');
-	    }
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-    }
 }
