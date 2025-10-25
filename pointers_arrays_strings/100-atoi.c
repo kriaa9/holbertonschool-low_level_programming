38a7c92221cf76c0155584cfd55de546549c7202
kriaa9 <11596@holbertonstudents.com>
2025-10-25 19:27:04 +0100

    Fix task 10: _atoi - handle all edge cases correctly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,40 @@
  */
 int _atoi(char *s)
 {
-	int i, sign, result, digit;
-
-	i = 0;
-	sign = 1;
-	result = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign *= -1;
+		{
+			if (!started)
+				sign *= -1;
+		}
+		else if (s[i] == '+')
+		{
+			if (!started)
+				sign *= 1;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
-			if (result > 0)
-				result = result * 10 + digit;
-			else
-				result = result * 10 - digit;
-
+			started = 1;
+			result = result * 10 - (s[i] - '0');
+			
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
+		}
+		else if (started)
+		{
+			break;
 		}
 		i++;
 	}
 
-	return (result * sign);
+	if (sign > 0)
+		result = -result;
+
+	return (result);
 }
