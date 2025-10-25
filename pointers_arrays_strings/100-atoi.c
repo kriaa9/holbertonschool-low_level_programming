19709e19ffae8193f4bcd18e5d4073cc248ca9a8
kriaa9 <11596@holbertonstudents.com>
2025-10-25 19:37:11 +0100

    Fix task 10: _atoi - correct Betty style errors

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,10 +8,12 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int started = 0;
+	int i, sign, result, started;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	started = 0;
 
 	while (s[i] != '\0')
 	{
@@ -29,19 +31,14 @@ int _atoi(char *s)
 		{
 			started = 1;
 			result = result * 10 - (s[i] - '0');
-			
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 		else if (started)
-		{
 			break;
-		}
 		i++;
 	}
-
 	if (sign > 0)
 		result = -result;
-
 	return (result);
 }
