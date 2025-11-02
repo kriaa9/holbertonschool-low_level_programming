6d7715054e646389d7ea70290a083ce6e078c759
kriaa9 <11596@holbertonstudents.com>
2025-11-02 20:48:39 +0100

    Add task 5: string_toupper - convert string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @str: String to modify
+ *
+ * Return: Pointer to modified string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+
+	return (str);
+}
