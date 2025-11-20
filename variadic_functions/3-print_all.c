be1a705e301c50d7357b8d3b63d2e7ed6031e7f0
kriaa9 <11596@holbertonstudents.com>
2025-11-20 22:06:43 +0100

    Fix Betty style: proper indentation and switch case alignment

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,25 +17,25 @@ void print_all(const char * const format, ...)
     {
         switch (format[i])
         {
-            case 'c':
-                printf("%s%c", sep, va_arg(args, int));
-                sep = ", ";
-                break;
-            case 'i':
-                printf("%s%d", sep, va_arg(args, int));
-                sep = ", ";
-                break;
-            case 'f':
-                printf("%s%f", sep, va_arg(args, double));
-                sep = ", ";
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (!str)
-                    str = "(nil)";
-                printf("%s%s", sep, str);
-                sep = ", ";
-                break;
+        case 'c':
+            printf("%s%c", sep, va_arg(args, int));
+            sep = ", ";
+            break;
+        case 'i':
+            printf("%s%d", sep, va_arg(args, int));
+            sep = ", ";
+            break;
+        case 'f':
+            printf("%s%f", sep, va_arg(args, double));
+            sep = ", ";
+            break;
+        case 's':
+            str = va_arg(args, char *);
+            if (!str)
+                str = "(nil)";
+            printf("%s%s", sep, str);
+            sep = ", ";
+            break;
         }
         i++;
     }
