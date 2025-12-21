8b7f950d9d4395a7dcae6de869e9081adff36e16
kriaa9 <11596@holbertonstudents.com>
2025-12-22 00:09:49 +0100

    File I/O: 1-create_file.c

---
+++ 1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file and writes text to it.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Calculate length of text_content if it exists */
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	/*
+	 * O_CREAT: Create file if it doesn't exist
+	 * O_RDWR: Open for reading and writing
+	 * O_TRUNC: Truncate file (clear it) if it already exists
+	 * 0600: Permissions rw------- (Read & Write for owner only)
+	 */
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
