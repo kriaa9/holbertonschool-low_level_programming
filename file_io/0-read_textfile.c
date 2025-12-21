106f14917699e5d535af133e5e590b3462342632
Abdallah Yessine Kriaa <11596@holbertonstudents.com>
2025-12-21 23:58:44 +0100

    Refactor error handling in read_textfile function
    Refactor error handling to improve readability and maintainability.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,33 +17,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 
 	if (filename == NULL)
-	    return (0);
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	    return (0);
+		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
-	    close(fd);
-	    return (0);
+		close(fd);
+		return (0);
 	}
 
 	r = read(fd, buf, letters);
 	if (r == -1)
 	{
-	    free(buf);
-	    close(fd);
-	    return (0);
+		free(buf);
+		close(fd);
+		return (0);
 	}
 
 	w = write(STDOUT_FILENO, buf, r);
 	if (w == -1 || w != r)
 	{
-	    free(buf);
-	    close(fd);
-	    return (0);
+		free(buf);
+		close(fd);
+		return (0);
 	}
 
 	free(buf);
