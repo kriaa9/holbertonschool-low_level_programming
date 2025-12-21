9afb9300e17ad8bea67e1b39eb47e49dbe9d62b3
kriaa9 <11596@holbertonstudents.com>
2025-12-22 00:54:22 +0100

    Fix header file and add Task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table to look into.
+ * @key: The key to look for.
+ *
+ * Return: The value associated with the element, or NULL if key not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
